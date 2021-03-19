a6050c46fb15e851860c7a0154481bdbe7ffce33
snowyuki31 <snow@DESKTOP-0M30J65.localdomain>
2021-03-20 05:38:06 +0900

    add weighted unionfind

--- snow/datastructure/weighted-unionfind.hpp
+++ snow/datastructure/weighted-unionfind.hpp
@@ -1,70 +1,84 @@
-// #pragma once
+#pragma once
 
-// #include <algorithm>
-// #include <cassert>
-// #include <vector>
+#include <algorithm>
+#include <cassert>
+#include <vector>
 
-// namespace snow {
+namespace snow {
 
-// // Based on ACL Implementation
-// struct WeightedUnionFind {
-//   public:
-//     dsu() : _n(0) {}
-//     explicit dsu(int n) : _n(n), parent_or_size(n, -1) {}
+// Based on ACL implementation
+template < typename Abel = int >
+struct WeightedUnionFind {
+  public:
+    WeightedUnionFind() : _n(0) {}
+    explicit WeightedUnionFind(int n, Abel e = 0) : _n(n), parent_or_size(n, -1), diff_weight(n, e) {}
 
-//     int merge(int a, int b) {
-//         assert(0 <= a && a < _n);
-//         assert(0 <= b && b < _n);
-//         int x = leader(a), y = leader(b);
-//         if (x == y) return x;
-//         if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
-//         parent_or_size[x] += parent_or_size[y];
-//         parent_or_size[y] = x;
-//         return x;
-//     }
+    bool merge(int a, int b, Abel w) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        w += weight(a), w -= weight(b);
+        int x = leader(a), y = leader(b);
+        if (x == y) return false;
+        if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y), w *= -1;
+        parent_or_size[x] += parent_or_size[y];
+        parent_or_size[y] = x;
+        diff_weight[y] = w;
+        return true;
+    }
 
-//     bool same(int a, int b) {
-//         assert(0 <= a && a < _n);
-//         assert(0 <= b && b < _n);
-//         return leader(a) == leader(b);
-//     }
+    bool same(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        return leader(a) == leader(b);
+    }
 
-//     int leader(int a) {
-//         assert(0 <= a && a < _n);
-//         if (parent_or_size[a] < 0) return a;
-//         return parent_or_size[a] = leader(parent_or_size[a]);
-//     }
+    int leader(int a) {
+        assert(0 <= a && a < _n);
+        if (parent_or_size[a] < 0) return a;
+        int r = leader(parent_or_size[a]);
+        diff_weight[a] += diff_weight[parent_or_size[a]];
+        return parent_or_size[a] = r;
+    }
 
-//     int size(int a) {
-//         assert(0 <= a && a < _n);
-//         return -parent_or_size[leader(a)];
-//     }
+    int size(int a) {
+        assert(0 <= a && a < _n);
+        return -parent_or_size[leader(a)];
+    }
 
-//     std::vector<std::vector<int>> groups() {
-//         std::vector<int> leader_buf(_n), group_size(_n);
-//         for (int i = 0; i < _n; i++) {
-//             leader_buf[i] = leader(i);
-//             group_size[leader_buf[i]]++;
-//         }
-//         std::vector<std::vector<int>> result(_n);
-//         for (int i = 0; i < _n; i++) {
-//             result[i].reserve(group_size[i]);
-//         }
-//         for (int i = 0; i < _n; i++) {
-//             result[leader_buf[i]].push_back(i);
-//         }
-//         result.erase(
-//             std::remove_if(result.begin(), result.end(),
-//                            [&](const std::vector<int>& v) { return v.empty(); }),
-//             result.end());
-//         return result;
-//     }
+    Abel weight(int a){
+        return diff_weight[leader(a)];
+    }
 
-//   private:
-//     int _n;
-//     // root node: -1 * component size
-//     // otherwise: parent
-//     std::vector<int> parent_or_size;
-// };
+    Abel diff(int a, int b){
+        return weight(b) - weight(a);
+    }
 
-// }  // namespace atcoder
+    std::vector<std::vector<int>> groups() {
+        std::vector<int> leader_buf(_n), group_size(_n);
+        for (int i = 0; i < _n; i++) {
+            leader_buf[i] = leader(i);
+            group_size[leader_buf[i]]++;
+        }
+        std::vector<std::vector<int>> result(_n);
+        for (int i = 0; i < _n; i++) {
+            result[i].reserve(group_size[i]);
+        }
+        for (int i = 0; i < _n; i++) {
+            result[leader_buf[i]].push_back(i);
+        }
+        result.erase(
+            std::remove_if(result.begin(), result.end(),
+                           [&](const std::vector<int>& v) { return v.empty(); }),
+            result.end());
+        return result;
+    }
+
+  private:
+    int _n;
+    // root node: -1 * component size
+    // otherwise: parent
+    std::vector<int> parent_or_size;
+    std::vector<Abel> diff_weight;
+};
+
+}  // namespace snow
--- test/aoj/DSL_1_B.test.cpp
+++ test/aoj/DSL_1_B.test.cpp
@@ -1,30 +1,30 @@
-// #define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_B"
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_B"
 
-// #include <iostream>
-// #include "snow/datastructure/weighted-unionfind.hpp"
+#include <iostream>
+#include "snow/datastructure/weighted-unionfind.hpp"
 
-// int main() {
-//     int n, q;
-//     std::cin >> n >> q;
+int main() {
+    int n, q;
+    std::cin >> n >> q;
 
-//     snow::WeightedUnionFind<int> wuf(n, 0);
+    snow::WeightedUnionFind<int> wuf(n, 0);
 
-//     while(q--){
-//         int t;
-//         std::cin >> t;
+    while(q--){
+        int t;
+        std::cin >> t;
 
-//         if(t == 0){
-//             int x, y, z;
-//             std::cin >> x >> y >> z;
-//             wuf.merge(x, y, z);
-//         }
-//         else{
-//             int x, y;
-//             std::cin >> x >> y;
-//             if(!wuf.same(x, y)) std::cout << '?' << '\n';
-//             else std::cout << wuf.diff(x, y) << '\n';
-//         }
-//     }
+        if(t == 0){
+            int x, y, z;
+            std::cin >> x >> y >> z;
+            wuf.merge(x, y, z);
+        }
+        else{
+            int x, y;
+            std::cin >> x >> y;
+            if(!wuf.same(x, y)) std::cout << '?' << '\n';
+            else std::cout << wuf.diff(x, y) << '\n';
+        }
+    }
 
-//     return 0;
-// }
+    return 0;
+}
