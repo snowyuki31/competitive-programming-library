e09fc9cc886d55790a326e85160e190ccdcd6e0f
snowyuki31 <root@DESKTOP-0M30J65.localdomain>
2021-03-20 19:23:05 +0900

    add fast sieve

---
+++ snow/math/simple-sieve.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+namespace snow {
+
+struct simple_sieve {
+    public:
+        simple_sieve(int n) : _n(n + 1), prime(_n, true) {
+            if(n >= 0) prime[0] = false;
+            if(n >= 1) prime[1] = false;
+            for(int i = 2; i * i <= n; ++i) if(prime[i]) {
+                for (int j = i * i; j <= n; j += i) prime[j] = false;
+            }
+        }
+
+        bool isprime(int n){
+            return !!prime[n];
+        }
+
+        std::vector<int> enumerate_prime() {
+            std::vector<int> prime_list;
+            prime_list.reserve(std::count(prime.begin(), prime.end(), true));
+            for(int i = 0; i < prime.size(); ++i) if(prime[i]) prime_list.emplace_back(i);
+            return prime_list;
+        }
+
+    protected:
+        int _n;
+        std::vector<bool> prime;
+};
+
+} // namespace snow
--- test/oj/enumerate_primes.test.cpp
+++ test/oj/enumerate_primes.test.cpp
@@ -1,15 +1,16 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/enumerate_primes"
 
 #include <iostream>
-#include "snow/math/sieve.hpp"
+#include "snow/math/simple-sieve.hpp"
 #include "snow/io/helper.hpp"
 
 int main() {
     int N, A, B;
     std::cin >> N >> A >> B;
 
-    snow::sieve sieve(N);
+    snow::simple_sieve sieve(N);
     auto ret = sieve.enumerate_prime();
+    
     std::vector<int> ans;
     for(int i = B; i < ret.size(); i += A){
         ans.emplace_back(ret[i]);
