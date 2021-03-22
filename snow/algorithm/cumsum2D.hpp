99d4277fb4e20a8f449e4f33b1b0ed048a0aaf8c
snowyuki31 <y-maeda.1031@keio.jp>
2021-03-23 05:30:21 +0900

    modify

--- snow/algorithm/cumsum2D.hpp
+++ snow/algorithm/cumsum2D.hpp
@@ -11,9 +11,9 @@ template < typename T >
 struct cumsum2D {
     public:
         cumsum2D(std::vector<std::vector<T>> const& data) : H(data.size()), W(data.front().size()), _data(H + 1, std::vector<T>(W + 1)) {
-            for (int i = 0; i <= H; ++i) {
-                for(int j = 0; j <= W; ++j) {
-                    if(i >= 1) _data[i][j] = data[i - 1][j - 1] + _data[i - 1][j];
+            for (int i = 1; i <= H; ++i) {
+                for(int j = 1; j <= W; ++j) {
+                    _data[i][j] = data[i - 1][j - 1] + _data[i - 1][j];
                 }
             }
             for (int i = 0; i <= H; ++i) {
