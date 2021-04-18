dc4255b56ca3e849667f4f5e9b2638a88ef4d37c
snowyuki31 <y-maeda.1031@keio.jp>
2021-04-19 02:46:08 +0900

    add lucas theorem

--- snow/math/convert-base.hpp
+++ snow/math/convert-base.hpp
@@ -3,26 +3,33 @@
 #include <algorithm>
 
 namespace snow {
-    /**
-     * @brief Base Conversion
-     * 
-     */
-    template <typename T>
-    std::vector<T> convert_base(T val, T base){
-        std::vector<T> ret;
-        T tmp = 1, base_ = abs(base);
-        while(val){
-            T dig = val * tmp % base_;
-            if(dig < 0) dig += base_;
-            ret.emplace_back(dig);
+/**
+ * @brief Base Conversion
+ * 
+ */
+template <typename T>
+std::vector<T> convert_base(T val, T base, int length = 0){
+    std::vector<T> ret;
+    T tmp = 1, base_ = abs(base);
+    while(val){
+        T dig = val * tmp % base_;
+        if(dig < 0) dig += base_;
+        ret.emplace_back(dig);
 
-            val -= dig * tmp;
-            val /= base_;
-            tmp *= base / base_;
-        }
-        if(ret.empty()) ret.emplace_back(0);
-        std::reverse(ret.begin(), ret.end());
+        val -= dig * tmp;
+        val /= base_;
+        tmp *= base / base_;
+    }
+    if(ret.empty()) ret.emplace_back(0);
 
-        return ret;
-    };
-};
+    if(length){
+        assert(ret.size() <= length);
+        while (ret.size() < length) ret.emplace_back(0);
+    }
+
+    std::reverse(ret.begin(), ret.end());
+
+    return ret;
+}
+
+} //namespace snow
