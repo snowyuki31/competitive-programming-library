7ddbd62fbb62f249c37ca641a5cb2ac2e58a8f5c
snowyuki31 <y-maeda.1031@keio.jp>
2021-04-21 03:08:47 +0900

    modify xorshift

--- snow/utils/xorshift.hpp
+++ snow/utils/xorshift.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <time.h>
 
 namespace snow {
 
@@ -7,13 +8,23 @@ namespace snow {
  */
 struct xorShift128 {
     unsigned long long x = 123456789, y = 362436069, z = 521288629, w = 88675123;
-    xorShift128(unsigned long long seed = 0) {
+    xorShift128(unsigned long long seed = time(NULL)) {
         w ^= seed;
     }
     unsigned long long operator()() {
         unsigned long long t = x ^ (x << 11);
         x = y; y = z; z = w;
         return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+    }
+
+    /**
+     * @brief Get random number in [l, r).
+     */
+    unsigned long long operator()(unsigned long long l, unsigned long long r) {
+        unsigned long long t = x ^ (x << 11);
+        x = y; y = z; z = w;
+        w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+        return w % (r - l) + l;
     }
 };
 
