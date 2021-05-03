deee2516fda1b0dc2d98daa9826ec9d25f9239ea
snowyuki31 <y-maeda.1031@keio.jp>
2021-05-03 16:24:51 +0900

    modify

--- snow/math/sieve.hpp
+++ snow/math/sieve.hpp
@@ -35,6 +35,7 @@ struct Sieve {
         }
 
         bool isprime(int x){
+            if(x == 0 or x == 1) return 0;
             return _sieve[x] == x;
         }
 
---
+++ snow/monoids/max.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "snow/utils/pair-wrapper.hpp"
+
+namespace snow {
+
+    template < typename T >
+    struct max_monoid {
+        using value_type = T;
+        static value_type e() { return std::numeric_limits<T>::min(); };
+        static value_type op(value_type l, value_type r){ return std::max(l, r); };
+
+        struct add {
+            using f_type = T;
+            static value_type mapping(f_type f, value_type x) { return f + x; }
+            static f_type composition(f_type f, f_type g) { return f + g; }
+            static f_type id(){ return T(); }
+        };
+
+        struct update {
+            struct f_type{
+                T val;
+                bool flag;
+            };
+            static value_type mapping(f_type f, value_type x) { return f.flag ? f.val : x; }
+            static f_type composition(f_type f, f_type g) { return f.flag ? f : g; }
+            static f_type id(){ return {T(), false}; }
+        };
+    };
+
+} // namespace snow
